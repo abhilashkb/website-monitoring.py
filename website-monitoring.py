f09475b9acaaa4369133d81c3a7b15ffd7623dcb
abhilashkb <abhilashkb9020@gmail.com>
2021-08-30 01:17:29 +0530

    added thread

--- website-monitoring.py
+++ website-monitoring.py
@@ -10,6 +10,7 @@ def status_code(domain):
     status_cod=str(requests.head('http://'+domain+'', headers={'User-Agent': 'Foo bar'},allow_redirects=True).status_code)
     if re.match( r'[2,3]\d{2}$' ,status_cod.strip()):
         print("status ok")
+
 j=0
 while True:
     if j < list_length:
