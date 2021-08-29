2fb0b0b6d8e6da1eb37bd676dd8a3f7ec4edd361
abhilashkb <abhilashkb9020@gmail.com>
2021-08-30 01:15:52 +0530

    first

--- website-monitoring.py
+++ website-monitoring.py
@@ -7,8 +7,22 @@ with open('domains-list.txt') as domains:
 list_length= len(domain_list)
 
 def status_code(domain):
-    status_code=str(requests.head('http://'+domain+'', headers={'User-Agent': 'Foo bar'},allow_redirects=True).status_code)
-    if re.match( r'[2,3]\d{2}$' ,status_code(d.strip())):
+    status_cod=str(requests.head('http://'+domain+'', headers={'User-Agent': 'Foo bar'},allow_redirects=True).status_code)
+    if re.match( r'[2,3]\d{2}$' ,status_cod.strip()):
         print("status ok")
-n=0
-for i in range(n,n+50)
+j=0
+while True:
+    if j < list_length:
+        threads= []
+        for i in range(j,j+50):
+            if i < list_length:
+                th=threading.Thread(target=status_code(domain_list[i].strip()))
+                th.start()
+                threads.append(th)
+        for t in threads:
+            t.join()
+        j=j+50    
+    else:
+        break
+
+
